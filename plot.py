10166c6047e51502c65b068a985f0b6edd8a7bf6
Richard Kirk <richkirk922@hotmail.com>
2024-11-06 15:49:31 +0100

    first commit

---
+++ plot.py
@@ -0,0 +1,5 @@
+
+
+
+def plot_csv(csv_filepath):
+    print("plot_csv" + str(csv_filepath))
---
+++ run_utility.py
@@ -0,0 +1,9 @@
+import plot
+
+
+if __name__ == "__main__":
+
+    print("utility start")
+
+    plot.plot_csv("daftfilepaht.csv")
+    
